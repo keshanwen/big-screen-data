cf6d6e9c429834ae811f18740125dc434f432ae5
keshanwen <3640@winnermedical.com>
2023-10-20 10:44:34 +0800

    feat: 引入 echart ，响应式demo

---
+++ src/views/map.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    hello wrold
+  </div>
+  </template>
+
+
+<script setup>
+
+
+</script>
---
+++ src/views/响应式demo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    hello wrold {{ name }}
+  </div>
+  <button @click="changeCb">change</button>
+  </template>
+
+
+<script setup>
+import { reactive, toRef, toRefs } from 'vue'
+
+
+const state = reactive({
+  name: 'kebi',
+  age: 24
+})
+
+// const { name } = state
+// const name = toRef(state, 'name')
+const { name } = toRefs(state)
+
+
+console.log(name)
+
+
+const changeCb = () => {
+  // state.name = 'change kebi'
+  name.value = 'i am change kebi'
+  console.log(state.name)
+}
+
+</script>
