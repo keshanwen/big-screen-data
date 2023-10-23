7b43664ff0f9e9d0022aa4730ac40b3b316b9b57
keshanwen <3640@winnermedical.com>
2023-10-23 16:23:28 +0800

    feat: echart 地图飞线设置

--- src/map/hook/initEchart.js
+++ src/map/hook/initEchart.js
@@ -1,36 +1,108 @@
-import { onUnmounted } from 'vue'
-import * as echarts from 'echarts'
-import registerMap from './registerMap'
-import { clickCb } from './event'
+import { onUnmounted } from 'vue';
+import * as echarts from 'echarts';
+import registerMap from './registerMap';
+import { clickCb } from './event';
+import chinaJson from '../utils/china.json';
 
 export const useInitEchart = (container, option = {}) => {
+  let echartInstance;
 
-  let echartInstance
+  if (!container) return;
 
-  if (!container) return
-
-  echartInstance = echarts.init(container)
+  echartInstance = echarts.init(container);
 
   // 注册地图
-  registerMap(echarts)
+  // registerMap(echarts)
+  echarts.registerMap('china', chinaJson); // 注册可用的地图
+  const coord = chinaJson.features.map((val) => {
+    return {
+      name: val.properties.name,
+      value: val.properties.cp,
+      visualMap: false,
+    };
+  });
+  const bejingCoord = coord.filter((item) => item.name === '北京');
+  const nobejingCoord = coord.filter((item) => item.name !== '北京');
+  const lines_coord = [];
+  coord.forEach((v, index) => {
+    if (v.name !== '北京') {
+      lines_coord.push({
+        coords: [v.value, bejingCoord[0].value],
+        visualMap: false,
+      });
+    }
+  });
+
+  option.series.push({
+    type: 'effectScatter',
+    coordinateSystem: 'geo',
+    zlevel: 15,
+    symbolSize: 12,
+    rippleEffect: {
+      period: 6,
+      brushType: 'stroke',
+      scale: 8,
+    },
+    itemStyle: {
+      color: '#FF5722',
+      opacity: 1,
+    },
+    data: bejingCoord,
+  });
+
+  option.series.push({
+    type: 'effectScatter',
+    coordinateSystem: 'geo',
+    zlevel: 15,
+    symbolSize: 8,
+    rippleEffect: {
+      period: 4,
+      brushType: 'stroke',
+      scale: 4,
+    },
+    itemStyle: {
+      color: '#FFB800',
+      opacity: 1,
+    },
+    data: nobejingCoord,
+  });
+
+  option.series.push({
+    type: 'lines',
+    coordinateSystem: 'geo',
+    zlevel: 15,
+
+   effect: {
+      show: true,
+      period: 5,
+      trailLength: 0,
+      symbol: 'arrow',
+      color: '#01AAED',
+      symbolSize: 8,
+    },
+    lineStyle: {
+        width: 1.2,
+        opacity: 0.6,
+        curveness: 0.2,
+       color: 'red',
+    },
+    data: lines_coord,
+  });
 
   // 监听事件
-  clickCb(echartInstance,echarts)
+  clickCb(echartInstance, echarts);
 
+  echartInstance.setOption(option);
 
-  echartInstance.setOption(option)
-
-
-  window.addEventListener('resize', function() {
+  window.addEventListener('resize', function () {
     echartInstance.resize();
   });
 
   onUnmounted(() => {
-    echartInstance. dispose()
-  })
-
+    echartInstance.dispose();
+  });
 
   return {
-    echartInstance
-  }
-}
+    echartInstance,
+  };
+};
--- src/map/hook/option.js
+++ src/map/hook/option.js
@@ -1,62 +1,96 @@
-
 export default (data) => {
-  return  {
-        // tooltip: { // 提示框组件
-        //   //数据格式化
-        //   formatter: function (params, callback) {
-        //     return (
-        //       params.seriesName + "<br />" + params.name + "：" + params.value
-        //     );
-        //   },
-        // },
-        visualMap: { // 视觉映射组件
-          min: 0,
-          max: 1000,
-          left: "left",
-          top: "bottom",
-          text: ["高", "低"], //取值范围的文字
-          inRange: {
-            color: ["#e0ffff", "blue"], //取值范围的颜色
-          },
-          show: true, //图注
+  return {
+    // tooltip: { // 提示框组件
+    //   //数据格式化
+    //   formatter: function (params, callback) {
+    //     return (
+    //       params.seriesName + "<br />" + params.name + "：" + params.value
+    //     );
+    //   },
+    // },
+    visualMap: {
+      // 视觉映射组件
+      min: 0,
+      max: 1000,
+      left: 'left',
+      top: 'bottom',
+      text: ['高', '低'], //取值范围的文字
+      inRange: {
+        color: ['#e0ffff', 'blue'], //取值范围的颜色
+      },
+      show: true, //图注
+    },
+    // visualMap: false,
+    geo: {
+      // 地理坐标系组件
+      map: 'china', //引入地图数据, 使用 registerMap 注册的地图名称。
+      zlevel: 10,
+      show: true,
+      layoutCenter: ['50%', '50%'],
+      roam: false,
+      layoutSize: '90%',
+      zoom: 1,
+      label: {
+       // normal: {
+          show: true,
+         // textStyle: {
+            fontSize: 12,
+            color: '#43D0D6',
+          //},
+        //},
+      },
+      itemStyle: {
+        //normal: {
+          color: '#062031',
+          borderWidth: 1.1,
+          borderColor: '#43D0D6',
+        //},
+      },
+      emphasis: {
+        areaColor: '#FFB800',
+        label: {
+          show: false,
         },
-        geo: { // 地理坐标系组件
-          map: 'china', //引入地图数据, 使用 registerMap 注册的地图名称。
-          roam: false, //不开启缩放和平移
-          zoom: 1, //视角缩放比例
-          label: { // 图形上的文本标签，可用于说明图形的一些数据信息，比如名称，值等.
-            normal: {
-              show: true,
-              fontSize: "10",
-              color: "rgba(0,0,0,0.7)",
-            },
-          },
-          itemStyle: { // 地图区域多边形样式
-            /*
-              normal 普通
-              emphasis 高亮
-            */
-            normal: {
-              borderColor: "rgba(0, 0, 0, 0.2)",
-            },
-            emphasis: { //高亮的显示设置
-              areaColor: "skyblue", //鼠标选择区域颜色。地图区域颜色
-              shadowOffsetX: 0,
-              shadowOffsetY: 0,
-              shadowBlur: 20,
-              borderWidth: 0,
-              shadowColor: "rgba(0, 0, 0, 0.5)",
-            },
-          },
-        },
-        // 鼠标悬浮提示框通过
-        series: [ // 系列列表。每个系列 type 决定自己的图表类型
-          {
-            name: "省份",
-            type: "map",
-            geoIndex: 0,
-            data,
-          },
-        ],
-      };
-}
+      },
+      // roam: false, //不开启缩放和平移
+      // zoom: 1, //视角缩放比例
+      // label: {
+      //   // 图形上的文本标签，可用于说明图形的一些数据信息，比如名称，值等.
+      //   normal: {
+      //     show: true,
+      //     fontSize: '10',
+      //     color: 'rgba(0,0,0,0.7)',
+      //   },
+      // },
+      // itemStyle: {
+      //   // 地图区域多边形样式
+      //   /*
+      //         normal 普通
+      //         emphasis 高亮
+      //       */
+      //   normal: {
+      //     borderColor: 'rgba(0, 0, 0, 0.2)',
+      //   },
+      //   emphasis: {
+      //     //高亮的显示设置
+      //     areaColor: 'skyblue', //鼠标选择区域颜色。地图区域颜色
+      //     shadowOffsetX: 0,
+      //     shadowOffsetY: 0,
+      //     shadowBlur: 20,
+      //     borderWidth: 0,
+      //     shadowColor: 'rgba(0, 0, 0, 0.5)',
+      //   },
+      // },
+    },
+    // 鼠标悬浮提示框通过
+    series: [
+      // 系列列表。每个系列 type 决定自己的图表类型
+      {
+        name: '省份',
+        type: 'map',
+        geoIndex: 0,
+        data,
+      },
+    ],
+  };
+};
