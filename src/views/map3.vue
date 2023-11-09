3357bbd54580f90192ab1b5c2e0783acfa88c798
keshanwen <3640@winnermedical.com>
2023-11-09 16:10:26 +0800

    feat: 区域层

--- src/views/map3.vue
+++ src/views/map3.vue
@@ -16,15 +16,17 @@ const main = () => {
   echarts.registerMap('china', chinaJson); // 注册可用的地图
 
   const data = [
+    // {
+    //   name: '西藏', value: 100, visualMap: false, label: {
+    //     show: true,
+    //     formatter(params) {
+    //       console.log(params, '6666')
+    //       return 24
+    //     }
+    //   }
+    // },
     {
-      name: '西藏', value: 100, visualMap: false, label: {
-        show: true,
-        formatter(params) {
-          console.log(params, '6666')
-          return 24
-        }
-      }
-},
+      name: '西藏', value: 100 },
     { name: '内蒙古', value: 200 },
     { name: '江西', value: 300 },
     { name: '陕西', value: 400 },
@@ -36,17 +38,117 @@ const main = () => {
     { name: '北京', value: 1000 },
   ];
 
-  const option = {
-    visualMap: { // 视觉映射组件
+  /*
+
+      连续区间
+
+      设置 visual 值时，请一次性全部设置，而非只设置一部分
+
+
+      区块样式
+
+      连续型区间
+      type: 'continuous',
       min: 0,
       max: 1000,
-      left: 'left',
-      top: 'bottom',
-      text: ['高', '低'], //取值范围的文字
+
       inRange: {
         color: ['#e0ffff', 'blue'], //取值范围的颜色
+        colorAlpha: [0,1], // 图元的颜色的透明度。
+      },
+
+      等分区间
+       type: 'piecewise',
+        min: 0,
+        max: 1000,
+       splitNumber: 4,
+      inRange: {
+         color: ['red','yellow' ,'blue', 'grey'],//取值范围的颜色. 图元的颜色。
+         colorAlpha: 1
+      }
+
+
+      自定义区间
+      type: 'piecewise',
+      pieces: [
+        { max: 500, color: 'grey' },
+        { min: 500, max: 600, color: 'yellow' },
+        { min: 600, max: 700, color: 'blue' },
+        { min: 700,  color: 'red' },
+      ],
+      inRange: {
+        colorAlpha: 1
+      },
+
+      数据标签
+      label: {
+        show: true,
+        fontSize: 12,
+        color: '#43D0D6',
+        formatter(params) {
+          const { name } = params
+          const obj = data.find(item => item.name === name)
+
+          if (obj) {
+            const { value } = obj
+            return `{a|${name}}\n{b|${value}}`
+          } else {
+            return ''
+          }
+        },
+        rich: {
+          a: {
+            color: 'white',
+            fontSize: 12,
+            fontStyle: 'italic',
+            // fontWeight: 'bolder'
+          },
+          b: {
+            color: 'red',
+            fontSize: 14,
+            fontWeight: 'bold'
+          },
+        }
+      },
+
+      // 图列
+      show: true, //图注
+      seriesIndex: 0,
+      showLabel: true, // 是否显示每项的文本标签
+      backgroundColor: '#ccc',
+      textStyle: {
+        color: 'red',
+        fontSize: 12,
+        fontWeight: 'bold',
+        fontStyle: 'oblique'
+      }
+  */
+
+  const option = {
+    visualMap: { // 视觉映射组件
+      type: 'piecewise',
+      pieces: [
+        { max: 500, color: 'grey' },
+        { min: 500, max: 600, color: 'yellow' },
+        { min: 600, max: 700, color: 'blue' },
+        { min: 700,  color: 'red' },
+      ],
+      left: 'left',
+      top: 'bottom',
+      text: ['订单金额', ''], //取值范围的文字
+      inRange: {
+        colorAlpha: 1
       },
       show: true, //图注
+      seriesIndex: 0,
+      showLabel: true, // 是否显示每项的文本标签
+      backgroundColor: '#ccc',
+      textStyle: {
+        color: 'red',
+        fontSize: 12,
+        fontWeight: 'bold',
+        fontStyle: 'oblique'
+      }
     },
     geo: {
       // 地理坐标系组件。 地理坐标系组件用于地图的绘制，支持在地理坐标系上绘制散点图，线集。
@@ -63,24 +165,37 @@ const main = () => {
         color: '#43D0D6',
         formatter(params) {
           const { name } = params
-          const arr = ['西藏','内蒙古','江西','陕西','山东', '福建','浙江','广东','上海','北京']
-          if (arr.includes(name)) {
-            return name
+          const obj = data.find(item => item.name === name)
+
+          if (obj) {
+            const { value } = obj
+            return `{a|${name}}\n{b|${value}}`
           } else {
             return ''
           }
+        },
+        rich: {
+          a: {
+            color: 'white',
+            fontSize: 12,
+            fontStyle: 'italic',
+            // fontWeight: 'bolder'
+          },
+          b: {
+            color: 'red',
+            fontSize: 14,
+            fontWeight: 'bold'
+          },
         }
       },
       itemStyle: {
         color: '#062031',
+        // areaColor: 'red',
         borderWidth: 1.1,
         borderColor: '#43D0D6',
       },
       emphasis: {
-        areaColor: '#FFB800',
-        label: {
-          show: true,
-        },
+        disabled : true,
       },
     },
     series: [
@@ -88,10 +203,10 @@ const main = () => {
         type: 'map', // 地图主要用于地理区域数据的可视化，配合 visualMap 组件用于展示不同区域的人口分布密度等数据。
        // name: '省份', // 系列名称，用于tooltip的显示，legend 的图例筛选，在 setOption 更新数据和配置项时用于指定对应的系列。
        // map: 'china', // 使用 registerMap 注册的地图名称。
-        geoIndex: 0, // 默认情况下，map series 会自己生成内部专用的 geo 组件。但是也可以用这个 geoIndex 指定一个 geo 组件。这样的话，map 和 其他 series（例如散点图）就可以共享一个 geo 组件了。并且，geo 组件的颜色也可以被这个 map series 控制，从而用 visualMap 来更改。 当设定了 geoIndex 后，series-map.map 属性，以及 series-map.itemStyle 等样式配置不再起作用，而是采用 geo 中的相应属性。
-        itemStyle: { // 地图区域的多边形 图形样式
-          color: 'yellow',
-        },
+       geoIndex: 0, // 默认情况下，map series 会自己生成内部专用的 geo 组件。但是也可以用这个 geoIndex 指定一个 geo 组件。这样的话，map 和 其他 series（例如散点图）就可以共享一个 geo 组件了。并且，geo 组件的颜色也可以被这个 map series 控制，从而用 visualMap 来更改。 当设定了 geoIndex 后，series-map.map 属性，以及 series-map.itemStyle 等样式配置不再起作用，而是采用 geo 中的相应属性。
+        // itemStyle: { // 地图区域的多边形 图形样式
+        //   color: 'yellow',
+        // },
         data,
       },
     ],
